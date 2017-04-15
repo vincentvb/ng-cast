36249f6d1fa3d26e694c3147ddda5196bd952ade
Hack Reactor Student <student@hackreactor.com>
2017-04-14 19:47:41 -0700

    onClick functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,17 @@
 angular.module('video-player')
 
+.controller('VideoData', function($scope) {
+	$scope.videos = window.exampleVideoData;
+	$scope.currentVideo = window.exampleVideoData[0];
+	$scope.currentVideoId = `https://www.youtube.com/embed/${$scope.currentVideo.id.videoId}`;
+	$scope.selectVideo = function(video) {
+		$scope.currentVideo = video;
+    $scope.currentVideoId = `https://www.youtube.com/embed/${$scope.currentVideo.id.videoId}`;
+	}
+})
 .directive('app', function() {
   return {
-	  templateUrl: 'src/templates/app.html'
+    templateUrl: 'src/templates/app.html'
   };
 })
-.controller('VideoData', function($scope) {
-	$scope.videoData = window.exampleVideoData;
-	$scope.currentVideo = window.exampleVideoData[0]
-	$scope.currentVideoId = `https://www.youtube.com/embed/${window.exampleVideoData[0].id.videoId}`;
-});
 
